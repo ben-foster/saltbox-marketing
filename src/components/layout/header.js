5580caab8339e830bcfc951b00ba19acf976af1f
Ben Foster <benfoster1093@gmail.com>
2020-01-31 14:11:50 -0500

    Separate header ctas from main header nav

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,14 +2,16 @@ import React, { useState } from "react"
 
 import HeaderNav from "./header-nav";
 import HeaderLogo from "./header-logo";
+import HeaderCTA from "./header-cta";
 
-const Header = ({ data }) => (
+const Header = () => (
     <header className="w-full shadow">
-        <div className="flex flex-row flex-wrap items-center px-2 py-8 container mx-auto">
+        <div className="flex flex-row flex-wrap items-center px-2 py-6 container mx-auto">
             <HeaderLogo />
             <HeaderNav />
+            <HeaderCTA />
         </div>  
     </header>
 )
 
-export default Header
+export default Header;
