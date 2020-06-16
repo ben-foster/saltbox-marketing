5cce4d11a1064136780bcbfff989485eebcfa9c4
Ben Foster <benfoster1093@gmail.com>
2020-06-16 17:53:15 -0400

    Remove whitepaper template, add query to job listing template

--- src/templates/job-listing.js
+++ src/templates/job-listing.js
@@ -10,4 +10,31 @@ const JobListing = ({ data }) => {
     );
 };
 
+
+export const query = graphql`
+	query($id: String!) {
+		wordpressWpJobListings(id: { eq: $id }) {
+			id
+			title
+            content
+			yoast_meta {
+                yoast_wpseo_title
+                yoast_wpseo_metadesc
+            }
+            featured_media {
+                alt_text
+                localFile {
+                    publicURL
+                    extension
+                    childImageSharp {
+                        fluid(maxWidth: 500, quality: 100) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+		}
+	}
+`
+
 export default JobListing;
