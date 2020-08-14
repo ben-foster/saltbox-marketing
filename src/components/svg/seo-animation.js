256b17cace4bc7b0d2f86eccdc22ff3957f7db15
ben-foster <benfoster1093@gmail.com>
2020-08-14 16:33:47 -0400

    Fix prev

--- src/components/svg/seo-animation.js
+++ src/components/svg/seo-animation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SEOAnimation = ({ className }) => {
+const SEOAnimation = ({ className, animateOnHover }) => {
     if(!animateOnHover) require("../script/seo-animation-intersection.js");
     return (  
         <svg className={className} width="628" height="353" viewBox="0 0 628 353" fill="none" xmlns="http://www.w3.org/2000/svg">
