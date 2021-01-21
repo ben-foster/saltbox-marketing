a420a45b88e4bac7c45281f01b8ceb4de50cdb7d
ben-foster <benfoster1093@gmail.com>
2021-01-21 15:13:42 -0500

    Inline GTM

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
 import Header from "./header";
 import Footer from "./footer";
@@ -7,6 +8,16 @@ import "./layout.css";
 
 const Layout = ({ children }) => (
     <>
+        <Helmet>
+            <script defer type="text/javascript">{`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WWXBKHH');
+            `}</script>
+        </Helmet>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWXBKHH" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Header />
         <div>
             <main>{children}</main>
