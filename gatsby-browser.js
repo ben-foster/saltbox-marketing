380eec1eafbb2f39da1ff5bb69e20b7a1f6a8b31
ben-foster <benfoster1093@gmail.com>
2021-01-21 15:43:23 -0500

    Remove GTM

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// Fire datalayer event when the client route changes
-exports.onRouteUpdate = ({ location, prevLocation }) => {
-  window.dataLayer.push({ event: 'gatsby-route-change' });
-}
-
 exports.onClientEntry = () => {
     window.addEventListener("load", () => {
       document.body.className = document.body.className.replace(/\bno-js\b/, "");
