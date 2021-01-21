f510faf4908a58a6d166df975b13832c243a2f72
ben-foster <benfoster1093@gmail.com>
2021-01-21 15:27:31 -0500

    Add route change handler that fires dataLayer event

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// Fire datalayer event when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  window.dataLayer.push({ event: 'gatsby-route-change' });
+}
+
 exports.onClientEntry = () => {
     window.addEventListener("load", () => {
       document.body.className = document.body.className.replace(/\bno-js\b/, "");
