48aedc8028e83e7ffc63ff98349ec0119baf560a
John F <johnrexter.flores@gmail.com>
2022-06-01 17:25:50 +0800

    Adjust banner on large siz

--- src/components/content/hero.js
+++ src/components/content/hero.js
@@ -5,7 +5,7 @@ import CurveBottom from "../svg/curve-bottom";
 const Hero = ({ children, className, fillColor, noCurve }) => {
     return (
         <div 
-            className={`relative z-10 ${className}`}
+            className={`relative z-10 p-8 ${className}`}
         >
             { children }
             {/* { !noCurve && (
