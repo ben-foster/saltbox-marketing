e7cc8d40d54a7f9d9fa5c723096e4a4e98e515bf
Ben Foster <benfoster1093@gmail.com>
2022-06-02 15:29:28 -0400

    Fix measurement id in prev

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
 			options: {
 			  // You can add multiple tracking ids and a pageview event will be fired for all of them.
 			  trackingIds: [
-				"250572659", // Google Analytics / GA
+				"G-0WF4LLTTSY", // Google Analytics / GA
 				// "AW-CONVERSION_ID", // Google Ads / Adwords / AW
 				// "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
 			  ],
