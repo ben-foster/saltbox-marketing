1f34af3977481425e5491f78505c6f785b72f6d3
Ben Foster <benfoster1093@gmail.com>
2022-06-26 23:27:24 -0400

    Add new favicon and upgrade postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/saltbox-logo-square-color.svg`, // This path is relative to the root of the site.
+				icon: `src/images/saltbox-logo-square.svg`, // This path is relative to the root of the site.
 			},
 		},
 		`gatsby-plugin-postcss`,
